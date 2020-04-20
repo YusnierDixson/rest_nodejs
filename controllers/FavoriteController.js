fb99e81f50d0720903f0cd41ef68e075f8c0dd80
YusnierDixson <yusnierdixson@gmail.com>
2020-04-20 13:58:27 +0200

    Virtual favoritos usuario

--- controllers/FavoriteController.js
+++ controllers/FavoriteController.js
@@ -1,5 +1,6 @@
 const buildParams = require('./helpers').buildParams;
 const FavoritePlace=require('../models/FavoritePlace');
+const User = require('../models/User');
 
 const validParams= ['_place'];
 
@@ -23,6 +24,18 @@ function find(req,res,next) {
 
 }
 
+function index(req,res){
+    User.findOne({'_id':req.user.id}).then(user=>{
+        console.log(user.places);
+        user.favorites.then(places=>{
+            res.json(places);
+        })
+    }).catch(err=>{
+        console.log(err);
+        res.json(err);
+    })
+}
+
 function destroy(req,res) {
     req.favorite.remove().then(doc=>{
         res.json({});
@@ -32,4 +45,4 @@ function destroy(req,res) {
   
 }
 
-module.exports={create,find,destroy};
+module.exports={create,find,destroy,index};
--- routes/favorites.js
+++ routes/favorites.js
@@ -2,10 +2,13 @@ var express = require('express');
 
 const favoriteController=require('../controllers/FavoriteController')
 const authenticateOwner=require('../middlewares/authenticateOwner');
+const jwtMiddleware=require('express-jwt');
+const secrets=require('../config/secrets');
 
 let router=express.Router();
 
 router.route('/')
+.get(jwtMiddleware({secret:secrets.jwtSecret}),favoriteController.index)
 .post(favoriteController.create);
 
 router.route('/:id')
