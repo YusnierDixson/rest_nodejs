dc7d7c996d10c171f97770af8f8d5843f603368f
YusnierDixson <yusnierdixson@gmail.com>
2020-04-20 15:53:32 +0200

    Visitas realizadas por un usuario

--- routes/visits.js
+++ routes/visits.js
@@ -2,12 +2,13 @@ var express = require('express');
 
 const visitController=require('../controllers/VisitController')
 const authenticateOwner=require('../middlewares/authenticateOwner');
-
+const jwtMiddleware=require('express-jwt');
+const secrets=require('../config/secrets');
 
 let router=express.Router();
 
 router.route('/')
-.get(visitController.index)
+.get(jwtMiddleware({secret:secrets.jwtSecret}),visitController.index)
 .post(visitController.create);
 
 router.route('/:visit_id')
